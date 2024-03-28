6badca600a9018e85871aced7ce1ade1a5c3319a
Raj <raj23jarwa@gmail.com>
2024-03-28 19:40:46 +0530

    Add some component

--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,21 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from './Signup'
+import Login from './Login'
+import Post from './Post'
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
 export {
     Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostCard,
+    PostForm
 }
